cb4ea247b0ee47655ef5eb2035691b444fc8009b
vinas <125512481+vinasxexplosao@users.noreply.github.com>
2026-06-02 23:14:59 -0300

    Delete Funcionalidade directory

--- Funcionalidade/script.js
+++
@@ -1,9 +0,0 @@
-const payloads = {
-    ps4: [
-        { name: 'GoldHEN 2.3', desc: 'Payload GoldHEN para PS4', file: 'goldhen_2.3.bin' },
-        { name: 'PS4Debug', desc: 'Ferramenta de debug para PS4', file: 'ps4debug.bin' }
-    ],
-    ps5: [
-        { name: 'PS5 Kernel Exploit', desc: 'Exploit para firmware 4.03/4.50', file: 'ps5_kexploit.bin' }
-    ]
-};
